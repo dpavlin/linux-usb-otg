2ea11cc0ce572ea7dc50482ecb55756886b9d574
Dobrica Pavlinusic <dpavlin@rot13.org>
2017-08-28 07:56:44 +0200

    remove gadget device

--- usb-hid-stop.sh
+++ usb-hid-stop.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 cd /sys/kernel/config/usb_gadget/g1
-echo "" > UDC
-rm configs/c.1/hid.usb0
-rmdir configs/c.1/strings/0x409
-rmdir configs/c.1
-rmdir strings/0x409
-rmdir functions/hid.usb0
+echo '' > UDC
+find configs -type l -exec rm -v {} \;
+#rmdir configs/c.1/strings/0x409
+find configs -name 'strings' -exec rmdir -v {}/0x409 \;
+#rmdir configs/c.1
+ls -d configs/* | xargs rmdir -v
+#rmdir strings/0x409
+ls -d strings/* | xargs rmdir -v
+#rmdir functions/hid.usb0
+ls -d functions/* | xargs rmdir -v
+
 cd ..
-rmdir g1
+rmdir -v g1
