c006460a488a869ac0750cd4a5007b6279451f01
Dobrica Pavlinusic <dpavlin@rot13.org>
2017-10-12 17:32:06 +0200

    how to install it

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+cp usb-otg.service /etc/systemd/system/
+systemctl enable usb-otg
+systemctl enable serial-getty@ttyGS0
+systemctl status usb-otg
+systemctl status serial-getty@ttyGS0
+
+# allow root login from gadget serial port
+grep ttyGS0 /etc/securetty || echo -e "# usb gadget serial\n/dev/ttyGS0\n" >> /etc/securetty
