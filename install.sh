a468ad81c23ae0a4de0c6969eecc35e777145c40
Dobrica Pavlinusic <dpavlin@rot13.org>
2017-10-12 17:51:44 +0200

    compile hid_gadget_test

--- install.sh
+++ install.sh
@@ -8,3 +8,5 @@ systemctl status serial-getty@ttyGS0
 
 # allow root login from gadget serial port
 grep ttyGS0 /etc/securetty || echo -e "# usb gadget serial\n/dev/ttyGS0\n" >> /etc/securetty
+
+make hid_gadget_test
